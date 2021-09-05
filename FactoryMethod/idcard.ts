da01c9873bb6ee8646fa829b3c42e4e3c9a5d5b3
Hyesung Oh <haesungoh414@gmail.com>
2021-09-05 15:55:16 +0900

    Factory method pattern

---
+++ FactoryMethod/idcard.ts
@@ -0,0 +1,35 @@
+import { Factory, Product } from "./framework";
+
+export class IDCard extends Product {
+    private owner: string;
+
+    constructor(owner: string) {
+        super();
+        console.log(`${owner}의 카드를 만듭니다.`);
+        this.owner = owner;
+    }
+
+    public use = (): void => {
+        console.log(`${this.owner}의 카드를 사용합니다.`);
+    };
+
+    public getOwner = (): string => {
+        return this.owner;
+    };
+}
+
+export class IDCardFactory extends Factory {
+    private owners: string[] = [];
+
+    protected createProduct = (owner: string): Product => {
+        return new IDCard(owner);
+    };
+
+    protected registerProduct = (product: Product): void => {
+        this.owners.push((product as IDCard).getOwner());
+    };
+
+    public getOwners = (): string[] => {
+        return this.owners;
+    };
+}
